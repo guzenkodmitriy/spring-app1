e94b96fe741919baa37c5ef538ff62a57b18d8fd
guzen <guzenko.dmitriy@gmail.com>
2024-12-14 18:26:24 +0200

    init-method, destroy-method, factory-method

--- src/main/java/com/guzenko/springcourse/TestSpring.java
+++ src/main/java/com/guzenko/springcourse/TestSpring.java
@@ -5,11 +5,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        ClassicalMusic classicalMusic = context.getBean("musicBeanClassical", ClassicalMusic.class);
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        System.out.println(classicalMusic.getSong());
+
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
