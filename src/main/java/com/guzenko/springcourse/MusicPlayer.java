e2356fe16700a7074f02abbdf0a81586f0f12fb0
guzen <guzenko.dmitriy@gmail.com>
2024-12-14 17:46:43 +0200

    List songs

--- src/main/java/com/guzenko/springcourse/MusicPlayer.java
+++ src/main/java/com/guzenko/springcourse/MusicPlayer.java
@@ -1,24 +1,32 @@
 package com.guzenko.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+
+    private List<Music> musicList = new ArrayList<Music>();
 
     private String name;
     private int volume;
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
     }
 
     public MusicPlayer() {}
 
-    public void setMusic(Music music) {
-        this.music = music;
-    }
 
     public void playMusic() {
-        System.out.println("Playing music: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing music: " + music.getSong());
+        }
+
     }
 
     public String getName() {
@@ -35,5 +43,9 @@ public class MusicPlayer {
 
     public void setVolume(int volume) {
         this.volume = volume;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 }
